948bed80093565165b6f2bbb207f0da053664633
venubabu <velpurivenubabu123@gmail.com>
2023-04-07 07:52:08 +0530

    [venu]venu

--- Program.cs
+++ Program.cs
@@ -5,11 +5,11 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-<<<<<<< HEAD
-            Console.WriteLine("Hello, World1!");
-=======
             Console.WriteLine("Hello, World!");
->>>>>>> b93fce9a02f2b2f8e357d9c998d811225514e562
+
+            Console.WriteLine("Hello, World1!");
+
+            
         }
     }
 }
